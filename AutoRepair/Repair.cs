ea0f32f27c25195f276f6b1fa030e3f6560ebf95
Wil -LaLonde <wil.lalonde@gmail.com>
2022-10-20 10:24:44 -0500

    Added Constructor To Repair Class

--- AutoRepair/Repair.cs
+++ AutoRepair/Repair.cs
@@ -6,8 +6,20 @@ using System.Threading.Tasks;
 
 namespace AutoRepair
 {
-    class Repair
+    public class Repair
     {
+        private double amount;
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public double Amount {
+            get { return amount; }
+            private set { amount = value; }
+        }
 
+        public Repair(DateTime date, string description, double amount) {
+            Date = date;
+            Description = description;
+            Amount = amount;
+        }
     }
 }
